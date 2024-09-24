7a41f261265c4eddf44f322f21e4e0638f85b30f
Andrés Martínez <autummata@gmail.com>
2024-09-24 00:09:01 -0600

    EV1: Parsed the contents of all files

--- hw/ev1/main.cpp
+++ hw/ev1/main.cpp
@@ -16,22 +16,29 @@ bool is_palindrome(std::string word) { // Two pointers approach
   return true;
 }
 
-int main() {
+std::vector<std::string> read_file(std::string file_name) {
   std::ifstream file;
-  file.open("transmission1.txt");
+  file.open(file_name);
+  std::vector<std::string> string_content;
   std::string file_output;
-  std::vector<std::string> transmission_one;
 
   if (file.is_open()) {
     while (file >> file_output) {
-      std::cout << file_output << std::endl;
-      transmission_one.push_back(file_output);
+      string_content.push_back(file_output);
     }
   }
-  std::cout << "Vector content" << std::endl;
+  return string_content;
+}
 
-  for (std::string line : transmission_one) {
-    std::cout << line << " ";
-  }
-  std::cout << std::endl;
+int main() {
+  std::vector<std::string> transmission_one = read_file("transmission1.txt");
+  std::vector<std::string> transmission_two = read_file("transmission2.txt");
+
+  std::string mcode_one = read_file("mcode1.txt")[0];
+  std::string mcode_two = read_file("mcode2.txt")[0];
+  std::string mcode_three = read_file("mcode3.txt")[0];
+
+  std::cout << mcode_one << std::endl;
+  std::cout << mcode_two << std::endl;
+  std::cout << mcode_three << std::endl;
 }
