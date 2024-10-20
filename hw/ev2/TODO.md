9a53ceb47b4f9f420e44d218e673c61f656bc427
Andrés Martínez <autummata@gmail.com>
2024-10-20 17:35:22 -0600

    Add: Floyd Warshall, TODO study md

---
+++ hw/ev2/TODO.md
@@ -0,0 +1,7 @@
+# Actividad Integradora 2
+## Pending to Study
+
+- Floyd Warshall Algorithm 
+- Travelling Salesman
+- Longest path between two nodes 
+- Coords? Possibly geometry? 
--- hw/ev2/main.cpp
+++ hw/ev2/main.cpp
@@ -1,20 +1,56 @@
 #include <iostream>
 #include <utility>
 #include <vector>
+
+
 void addEdge(std::vector<std::pair<int, int> > adj[], int u, int v, int wt) {
-  for (const auto& edge : adj[u]) {
+  for (const auto &edge : adj[u]) {
     if (edge.first == v && edge.second == wt) {
-      return; // Edge already exists, do not add it again
+      return; // If edge already exists, don't add it again : )
     }
   }
   adj[u].push_back(std::make_pair(v, wt));
   adj[v].push_back(std::make_pair(u, wt));
 }
 
+/**
+ * @brief https://www.geeksforgeeks.org/floyd-warshall-algorithm-dp-16/
+ * 
+ * @param V 
+ * @return std::vector<std::vector<int> > 
+ */
+
+std::vector<std::vector<int> > floyd_warshall(std::vector<std::pair<int, int> >adj[], int V) {
+    std::vector<std::vector<int> > dist(V, std::vector<int>(V, INT_MAX));
+
+    for (int i = 0; i < V; i++) {
+        dist[i][i] = 0; // Distance from a vertex to itself is 0
+        for (const auto& edge : adj[i]) {
+            int j = edge.first;
+            int weight = edge.second;
+            dist[i][j] = weight;
+        }
+    }
+
+    // Floyd-Warshall algorithm
+    for (int k = 0; k < V; k++) {
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (dist[i][k] != INT_MAX && dist[k][j] != INT_MAX &&
+                    dist[i][k] + dist[k][j] < dist[i][j]) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+
+    return dist;
+}
+
 void printGraph(std::vector<std::pair<int, int> > adj[], int V) {
   int v, w;
   for (int u = 0; u < V; u++) {
-    std::cout << u << "\n"; 
+    std::cout << u << "\n";
     for (auto it = adj[u].begin(); it != adj[u].end(); it++) {
       v = it->first;
       w = it->second;
@@ -40,8 +76,21 @@ int main() {
       }
     }
   }
-  
 
   printGraph(adj, V);
+
+
+  std::vector<std::vector<int> > dist = floyd_warshall(adj, V);
+  for (int i = 0; i < V; i++) {
+      for (int j = 0; j < V; j++) {
+          if (dist[i][j] == INT_MAX) {
+              std::cout << "INF ";
+          } else {
+              std::cout << dist[i][j] << " ";
+          }
+      }
+      std::cout << "\n";
+  }
+
   return 0;
 }
